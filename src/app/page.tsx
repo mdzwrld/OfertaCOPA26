0116e6dc0f134b942b999620cfe3d9753014cd31
Josuehd Snjsjiaia <midszreformes@gmail.com>
2026-03-06 21:22:03 +0000

    MOVEisso:
    Você não precisa ter seguidores para vender camisas de time.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Vsl />
-        <ProfitSection />
         <Testimonials />
+        <ProfitSection />
         <Offer />
         <Opportunity />
       </main>
