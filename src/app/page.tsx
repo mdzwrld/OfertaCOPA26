d1fa12f89a3e363903993feaa7ec293fe24e40ac
Josuehd Snjsjiaia <midszreformes@gmail.com>
2026-03-06 06:03:25 +0000

    remova o head da pagina!
    troque essa forte, quero algo parecido como da

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-import { Benefits } from "@/components/landing/benefits";
 import { Footer } from "@/components/landing/footer";
-import { Header } from "@/components/landing/header";
 import { Hero } from "@/components/landing/hero";
 import { Offer } from "@/components/landing/offer";
 import { ProfitSection } from "@/components/landing/profit-section";
 import { StickyCta } from "@/components/landing/sticky-cta";
 import { Testimonials } from "@/components/landing/testimonials";
+import { UrgencyCta } from "@/components/landing/urgency-cta";
 import { Vsl } from "@/components/landing/vsl";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-1">
         <Hero />
         <Vsl />
-        <Benefits />
+        <UrgencyCta />
         <ProfitSection />
         <Testimonials />
         <Offer />
